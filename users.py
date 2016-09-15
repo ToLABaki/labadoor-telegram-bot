065940baad7bc9dcb53d9020d3b7f8ca3e285a98
George Kaklamanos <gkaklas1@gmail.com>
2016-09-15 21:44:12 +0300

    Adjust users.py to fit in Labaperson context.

--- users.py
+++ users.py
@@ -4,38 +4,38 @@ class Labaperson:
     def __init__(self, id):
         self._id = id
 
-    def get_id():
+    def get_id(self):
         return self._id
 
-    def is_admin(db, uid):
-        role = False
-        db.connect()
-        for user in User.select().where(User.uid==uid):
-            role = user.admin
-        db.close() 
-        return True
-
-    def is_user(db, uid):
+    def is_admin(self, db):
         found = False
         db.connect()
-        found = User.select().where(User.uid==uid).exists()
+        for user in User.select().where(User.uid == self.get_id()):
+            found = user.admin
+        db.close() 
+        return role
+
+    def is_user(self, db):
+        found = False
+        db.connect()
+        found = User.select().where(User.uid == self.get_id()).exists()
         db.close() 
         return found
 
-    def add_user(db, uid):
+    def add_user(self, db):
         st = False
         db.connect()
         if not is_user(db, uid):
-            User(uid=uid, admin=False).save()
+            User(uid = self.get_id(), admin=False).save()
             st = True
         db.close()
         return st
 
-    def del_user(db, uid):
+    def del_user(self, db):
         found = False
         db.connect()
         if str(uid).isdigit():
-            for user in User.select().where(User.uid==uid):
+            for user in User.select().where(User.uid == self.get_id()):
                 user.delete_instance()
                 found = True
                 break
