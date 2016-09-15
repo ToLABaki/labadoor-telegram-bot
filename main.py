3a05d83716da27164789eea13ede809a5b2b590c
George Kaklamanos <gkaklas1@gmail.com>
2016-09-15 19:37:09 +0300

    Use Labaperson in FSM.

--- main.py
+++ main.py
@@ -18,124 +18,131 @@ class FSM:
     def get_state(self, state):
         return self._state
 
+    def set_person(self, person):
+        self._person = person
+
+    def get_person(self):
+        return self._person
+
     # Message handler. 
     # Don't change function name, it is recognized by default from telepot.
     def on_chat_message(self, msg):
         cmd = msg['text']
-        chat_id = msg['chat']['id']
+        self.set_person(Labaperson(msg['chat']['id']))
         global db
         global markup
 
         # Set keyboard depending on the role of the user.
-        if is_admin(db, chat_id):
+        if self.get_person().is_admin(db):
             markup = keyboard_admin
-        elif is_user(db, chat_id):
+        if self.get_person().is_user(db):
             markup = keyboard_user
         else:
             markup = keyboard_guest
 
 
         if self.get_state() == state.lockdown:
-            bot.sendMessage(chat_id, str_botdisabled, reply_markup=markup)
+            bot.sendMessage(self.get_person().get_id(), str_botdisabled,
+                    reply_markup=markup)
 
 
         elif self.get_state() == state.guestopen:
-            guest_open(db, bot, chat_id, cmd)
+            self.guest_open(db, bot, cmd)
             self.set_state(state.start)
 
 
         elif self.get_state() == state.deltoken:
             if delete_token(db, cmd):
-                bot.sendMessage(chat_id, 'Token ' + cmd +' deleted.',
-                        reply_markup=markup)
+                bot.sendMessage(self.get_person().get_id(), 'Token ' + cmd + 
+                        ' deleted.', reply_markup=markup)
             else:
-                bot.sendMessage(chat_id, cmd +': token isn\'t registered',
-                        reply_markup=markup)
+                bot.sendMessage(self.get_person().get_id(), cmd + 
+                        ': token isn\'t registered', reply_markup=markup)
             self.set_state(state.start)
 
 
         elif self.get_state() == state.adduser:
             if add_user(db, cmd):
-                bot.sendMessage(chat_id, 'User ' + cmd + ' added.',
-                        reply_markup=markup)
+                bot.sendMessage(self.get_person().get_id(), 'User ' + cmd + 
+                        ' added.', reply_markup=markup)
             else:
-                bot.sendMessage(chat_id, 'User ' + cmd + 
+                bot.sendMessage(self.get_person().get_id(), 'User ' + cmd + 
                         ' is already registered.', reply_markup=markup)
             self.set_state(state.start)
 
 
         elif self.get_state() == state.deluser:
             if del_user(db, cmd):
-                bot.sendMessage(chat_id, 'User ' + cmd +' deleted.',
-                        reply_markup=markup)
+                bot.sendMessage(self.get_person().get_id(), 'User ' + cmd + 
+                        ' deleted.', reply_markup=markup)
             else:
-                bot.sendMessage(chat_id, cmd +': user isn\'t registered',
-                        reply_markup=markup)
+                bot.sendMessage(self.get_person().get_id(), cmd + 
+                        ': user isn\'t registered', reply_markup=markup)
             self.set_state(state.start)
 
         elif self.get_state() == state.start:
             self.start()
 
     # Welcome messages, sent when user start a chat with the bot (/start).
-    def welcome(bot, chat_id):
+    def welcome(bot):
         global welcome_messages
         for msg in welcome_messages:
-            bot.sendMessage(chat_id, msg, reply_markup=keyboard_hide)
+            bot.sendMessage(self.get_person().get_id, msg, reply_markup=keyboard_hide)
             sleep(3)
 
 
     def start():
             try:
                 if cmd == '/start':
-                    welcome(bot, chat_id)
+                    self.welcome(bot)
 
                 elif cmd == '/open':
-                    self.set_state(open(db, bot, chat_id, None))
+                    self.set_state(open(db, bot, None))
 
                 elif cmd == '/gettoken':
-                    gettoken(db, bot, chat_id)
+                    gettoken(db, bot, self.get_person().get_id())
                     self.set_state(state.start)
 
                 elif cmd == '/deltoken':
-                    self.set_state(deltoken(db, bot, chat_id))
+                    self.set_state(deltoken(db, bot, self.get_person().get_id()))
 
                 elif cmd == '/tokens':
-                    self.ShowTokens(db, bot, chat_id)
+                    self.ShowTokens(db, bot)
 
                 elif cmd == '/adduser':
-                    self.set_state(adduser(db, bot, chat_id))
+                    self.set_state(adduser(db, bot))
 
                 elif cmd == '/deluser':
-                    self.set_state(deluser(db, bot, chat_id))
+                    self.set_state(deluser(db, bot))
 
                 elif cmd == '/users':
-                    self.ShowUsers(db, bot, chat_id)
+                    self.ShowUsers(db, bot)
                     self.set_state(state.start)
 
                 elif cmd == '/lock':
-                    if is_admin(db, chat_id):
+                    if self.get_person().is_admin(db):
                         self.set_state(state.lockdown)
                     else:
-                        bot.sendMessage(chat_id, 
+                        bot.sendMessage(self.get_person().get_id(), 
                                 'Only admins can lock the bot.',
                                 reply_markup=markup)
                 elif cmd == '/unlock':
-                    if is_admin(db, chat_id):
+                    if self.get_person().is_admin(db):
                         self.set_state(state.normal)
                     else:
-                        bot.sendMessage(chat_id, 
+                        bot.sendMessage(self.get_person().get_id(), 
                                 'Only admins can unlock the bot.',
                                 reply_markup=markup)
                 else:
                     print(cmd,st)
             except LabadoorBotException as e:
-                bot.sendMessage(chat_id, e.string, reply_markup=markup)
+                bot.sendMessage(self.get_person().get_id(), e.string, reply_markup=markup)
                 
-    def adduser(db, bot, chat_id):
+    def adduser(db, bot):
         self.set_state(state.start)
-        if is_admin(db, chat_id):
+        if self.get_person().is_admin(db):
             self.set_state(state.adduser)
-            bot.sendMessage(chat_id, str_adduser, reply_markup=markup)
+            bot.sendMessage(self.get_person().get_id(), str_adduser, reply_markup=markup)
         else:
             raise EditUsersException
         return st
@@ -143,9 +150,9 @@ class FSM:
     def deluser(db, bot, chat_id):
         global st
         self.set_state(state.start)
-        if is_admin(db, chat_id):
+        if self.get_person().is_admin(db):
             self.set_state(state.deluser)
-            bot.sendMessage(chat_id, str_deluser, reply_markup=markup)
+            bot.sendMessage(self.get_person().get_id(), str_deluser, reply_markup=markup)
         else:
             raise EditUsersException
         return st
@@ -153,37 +160,37 @@ class FSM:
     # Ask the user for the token they want to delete.
     def deltoken(db, bot, chat_id):
         self.set_state(state.start)
-        if is_user(db, chat_id):
-            bot.sendMessage(chat_id, str_deltoken, reply_markup=markup)
+        if self.get_person().is_user(db):
+            bot.sendMessage(self.get_person().get_id(), str_deltoken, reply_markup=markup)
             self.set_state(state.deltoken)
         else:
             raise EditTokensException
         return st
 
     # Print a list of all the users.
-    def ShowUsers(db, bot, chat_id):
-        if is_admin(db, chat_id):
+    def ShowUsers(db, bot):
+        if self.get_person().is_admin(db):
             db.connect()
             users = User.select()
             if users.count()==0:
-                bot.sendMessage(chat_id, "No users.", reply_markup=markup)
+                bot.sendMessage(self.get_person().get_id(), "No users.", reply_markup=markup)
             else:
                 for user in users:
-                    bot.sendMessage(chat_id, str(user.uid), reply_markup=markup)
+                    bot.sendMessage(self.get_person().get_id(), str(user.uid), reply_markup=markup)
             db.close() 
         else:
             raise ShowUsersException
 
     # Print all valid tokens
-    def ShowTokens(db, bot, chat_id):
-        if is_admin(db, chat_id):
+    def ShowTokens(db, bot):
+        if self.get_person().is_admin(db):
             db.connect()
             tokens = Token.select().where(Token.valid==True)
             if tokens.count()==0:
-                bot.sendMessage(chat_id, "No tokens", reply_markup=markup)
+                bot.sendMessage(self.get_person().get_id(), "No tokens", reply_markup=markup)
             else:
                 for token in tokens:
-                    bot.sendMessage(chat_id, str(token.token),
+                    bot.sendMessage(self.get_person().get_id(), str(token.token),
                             reply_markup=markup)
             db.close() 
         else:
@@ -193,23 +200,23 @@ class FSM:
     # If a user tries to open the door, open it.
     # However, if a non-user wants to open the door, change the state in order
     # to ask for an access token.
-    def open(db, bot, chat_id, token):
+    def open(db, bot, token):
         global markup
-        if is_user(db, chat_id):
-            bot.sendMessage(chat_id,'Open', reply_markup=markup)
+        if self.get_person().is_user(db):
+            bot.sendMessage(self.get_person().get_id(),'Open', reply_markup=markup)
             # Replace this comment with the command that actually opens the door
             self.set_state(state.start)
         else:
-            bot.sendMessage(chat_id, str_guestoops, reply_markup=markup)
+            bot.sendMessage(self.get_person().get_id(), str_guestoops, reply_markup=markup)
             self.set_state(state.guestopen)
         return st
 
     # Open the door if the provided access token is correct.
-    def guest_open(db, bot, chat_id, token):
+    def guest_open(db, bot, token):
         if auth_token(db, token):
-            bot.sendMessage(chat_id,'Open', reply_markup=markup)
+            bot.sendMessage(self.get_person().get_id(),'Open', reply_markup=markup)
         else:
-            bot.sendMessage(chat_id,'Invalid token', reply_markup=markup)
+            bot.sendMessage(self.get_person().get_id(),'Invalid token', reply_markup=markup)
 
 
 if __name__ == "__main__":
