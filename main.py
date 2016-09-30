1528bc9c43b8088e71fc3ff51ef150f9306bc301
Constantine K <looselyrigorous@tolabaki.gr>
2016-09-30 14:26:08 +0300

    integrate "vars.py" into main file

--- main.py
+++ main.py
@@ -2,12 +2,67 @@
 
 import telepot
 from telepot.delegate import pave_event_space, per_chat_id, create_open
+from telepot.namedtuple import ReplyKeyboardMarkup, ReplyKeyboardHide
 from time import sleep
+import peewee
+from peewee import Model, IntegerField, BooleanField, CharField
 from api_key import *
 from exceptions import *
 from labatoken import *
 from labaperson import *
-from vars import *
+
+DB_PATH = 'stuff.db'
+
+keyboard_user = ReplyKeyboardMarkup(keyboard=[
+    ['/open'],
+    ['/gettoken', '/deltoken']
+    ])
+
+keyboard_admin = ReplyKeyboardMarkup(keyboard=[
+    ['/open', '/users', '/tokens'],
+    ['/gettoken', '/deltoken'],
+    ['/adduser', '/deluser'],
+    ['/lock', '/unlock']
+    ])
+
+keyboard_guest = ReplyKeyboardMarkup(keyboard=[
+    ['/open']
+    ])
+
+keyboard_hide = ReplyKeyboardHide(hide_keyboard=True)
+
+str_guestoops = 'Oops, you\'re a guest so you must send me a password now'
+str_heresatoken = 'This code will allow a friend to open the door one time:'
+str_botdisabled = 'The bot is disabled for security reasons.'
+str_deltoken = 'Send me the token you need cancelled.'
+str_adduser = 'Send me the user id you need to register.'
+str_deluser = 'Send me the user id you need to remove.'
+
+welcome_messages = [
+    "Oh, hey there!",
+    "This bot helps us open the door at τοLabάκι hackerspace.",
+    "You need to be registered by an admin in order to use the bot.",
+    "Alternatively, you can also get a one-time access password from a registered \
+    member.",
+    "For more info, you can visit http://wiki.tolabaki.gr/w/To_LABaki",
+    "Enjoy your stay!"
+]
+
+db = peewee.SqliteDatabase(DB_PATH)
+
+class Token(Model):
+    token = CharField()
+
+    class Meta:
+        database = db
+
+
+class User(Model):
+    uid = IntegerField()
+    admin = BooleanField()
+
+    class Meta:
+        database = db
 
 
 class FSM(telepot.helper.ChatHandler):
@@ -25,8 +80,6 @@ class FSM(telepot.helper.ChatHandler):
     def on_chat_message(self, msg):
         cmd = msg['text']
         self.set_person(Labaperson(msg['chat']['id']))
-        global db
-        global markup
 
         # Set keyboard depending on the role of the user.
         if self.get_person().is_admin(db):
@@ -196,6 +249,9 @@ class FSM(telepot.helper.ChatHandler):
         else:
             bot.sendMessage(self.get_person().get_id(),'Invalid token', reply_markup=markup)
 
+    db.connect()
+    db.create_tables([Token, User], safe=True)
+    db.close()
 
 if __name__ == "__main__":
 
